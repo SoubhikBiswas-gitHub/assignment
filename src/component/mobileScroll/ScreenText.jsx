0b72268362b491a93fbe75be1b58e1e3046833d8
SOUBHIK BISWAS <sbiswas773@gmail.com>
2022-10-31 12:44:19 +0530

    intersection Observe API upadate

--- src/component/mobileScroll/ScreenText.jsx
+++ src/component/mobileScroll/ScreenText.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useState } from "react";
 
 const ScreenText = ({ data, index, setImgIndex }) => {
   const ref = useRef(null);
-  console.log(ref)
   const [textVisibility, setTextVisibility] = useState(false);
 
   const options = {
@@ -11,13 +10,13 @@ const ScreenText = ({ data, index, setImgIndex }) => {
     threshold: 0.6,
   };
 
-  const observerText = (e) => {
-    if (e[0]?.isIntersecting) {
+  const observerText = (entries) => {
+    const [entry] = entries;
+    if (entry.isIntersecting) {
       setTextVisibility(!textVisibility);
-      setImgIndex(index)
+      setImgIndex(index);
     }
   };
-
   useEffect(() => {
     const observar = new IntersectionObserver(observerText, options);
     if (!textVisibility) {
@@ -26,23 +25,24 @@ const ScreenText = ({ data, index, setImgIndex }) => {
       }
     }
 
-    return ()=>{
-      if(ref.current){
-        observar.unobserve(ref.current)
+    return () => {
+      if (ref.current) {
+        observar.unobserve(ref.current);
       }
-    }
-  },[]);
-
- 
+    };
+  }, []);
 
   return (
-    <div className={`screen-text ${textVisibility?"text-visible":""}`} ref={ref}>
+    <div
+      className={`screen-text ${textVisibility ? "text-visible" : ""}`}
+      ref={ref}
+    >
       <div className="screen-heading">{data.heading}</div>
       <div className="only-mobile mobile-wrapper">
         <div className="mobile">
           <div className="mobile-screen flex absolute-center">
             <img
-            key={data.imgURL}
+              key={data.imgURL}
               src={data.imgURL}
               alt="img"
               className="mobile-img slide-in-right"
